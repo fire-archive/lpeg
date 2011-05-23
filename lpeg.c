ea2a80fc253f3411cdbcfd6e07ff8a55c597a8f0
Ernest Lee <fire@chibifire.com>
2011-05-23 11:06:42 -0700

    Make compatible with windows.

--- lpeg.c
+++ lpeg.c
@@ -2388,8 +2388,8 @@ static struct luaL_Reg metapattreg[] = {
 };
 
 
-int luaopen_lpeg (lua_State *L);
-int luaopen_lpeg (lua_State *L) {
+LUALIB_API int luaopen_lpeg (lua_State *L);
+LUALIB_API int luaopen_lpeg (lua_State *L) {
   lua_pushcfunction(L, (lua_CFunction)&l_newpf);  /* new-pattern function */
   lua_setfield(L, LUA_REGISTRYINDEX, KEYNEWPATT);  /* register it */
   luaL_newmetatable(L, PATTERN_T);
